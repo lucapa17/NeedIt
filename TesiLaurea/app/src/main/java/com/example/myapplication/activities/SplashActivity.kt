8c116aaddb3abf3f3a2c093feb448b2259f6bf2f
GiacomoBisio <giacomo31200@gmail.com>
2022-07-11 12:54:46 +0200

    Login/Reg_11_07

--- TesiLaurea/app/src/main/java/com/example/myapplication/activities/SplashActivity.kt
+++ TesiLaurea/app/src/main/java/com/example/myapplication/activities/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.models.FireBaseWrapper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,11 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         // TODO: check if user logged or not
+        val firebaseWrapper : FireBaseWrapper = FireBaseWrapper()
+        if (!firebaseWrapper.isAuthenticated()) {
+            //redirect to login/register activity
+            val intent = Intent(this, LoginActivity::class.java)
+        }
 
         //Start Main Activity
         val intent = Intent(this, MainActivity::class.java)
---
+++ TesiLaurea/app/src/main/java/com/example/myapplication/fragments/LogFragment.kt
@@ -0,0 +1,92 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.models.FireBaseWrapper
+import com.google.firebase.auth.FirebaseAuth
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LogFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+abstract class LogFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+        }*/
+
+
+    }
+
+    /*override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }*/
+
+    abstract fun action(email: String, password: String)
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LogFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
+
+class LogInFragment : LogFragment() {
+    override fun action(email: String, password: String) {
+        val firebaseWrapper : FireBaseWrapper = FireBaseWrapper()
+        firebaseWrapper.signIn(email, password)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }
+}
+
+class SignUpFragment : LogFragment() {
+    override fun action(email: String, password: String) {
+        val firebaseWrapper : FireBaseWrapper = FireBaseWrapper()
+        firebaseWrapper.signUp(email, password)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }
+}
